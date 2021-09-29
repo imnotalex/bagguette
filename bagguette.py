739140e9d9a0ce22854a6f1061e761247c3f6c9a
imnotalex <91630766+imnotalex@users.noreply.github.com>
2021-09-29 14:07:50 -0500

    the code and image
    very cool amiright?

---
+++ bagguette.py
@@ -0,0 +1,14 @@
+#import image module
+from PIL import Image as bagguette
+import os
+
+imagebayby = "36839.jpg"
+ime = bagguette.open(imagebayby)
+#infinite loop variable
+#infinite loop
+loop = 0
+while(loop <= 3):
+  loop = loop + 1
+  ime.show()
+  
+        
