5a81c5921d8c5890ae0c6c63af44d0cfc3be7bbf
imnotalex <91630766+imnotalex@users.noreply.github.com>
2021-09-29 14:20:10 -0500

    make better for test mode luv ya ;;;;;))));;;;;)))

--- bagguette.py
+++ bagguette.py
@@ -4,11 +4,19 @@ import os
 
 imagebayby = "36839.jpg"
 ime = bagguette.open(imagebayby)
+
 #infinite loop variable
-#infinite loop
 loop = 0
-while(loop <= 3):
+#number of times (test mode only)
+verycoolinput = input("how many times should bagguette(this is only a test node fature)")
+print(type(verycoolinput))
+verycoolinput = int(verycoolinput)
+print(type(verycoolinput))
+#loop
+while(loop <= verycoolinput):
   loop = loop + 1
+  #show image
   ime.show()
   
         
+#thats all the code bitch
